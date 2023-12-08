84d82e5c93ae80c08952efc2f17cb81e6844e771
sayed ahmed <sayed.ahmed.elbadry@gmail.com>
2023-12-08 23:36:26 +0200

    updated

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -1,23 +1,36 @@
 <?php
 return [
+     /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => false,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
     'role_structure' => [
-        'superadministrator' => [
+        'manger' => [
             'users' => 'c,r,u,d',
-            'acl' => 'c,r,u,d',
-            'profile' => 'r,u'
+            'students' => 'c,r,u,d',
+            'courses' => 'c,r,u,d',
+            'listens' => 'c,r,u,d',
+            'subscriptions' => 'c,r,u,d',
         ],
-        'administrator' => [
-            'users' => 'c,r,u,d',
-            'profile' => 'r,u'
-        ],
-        'user' => [
-                'profile' => 'r,u'
-        ],
+        'teacher' => [],
+        'assistant' => [],
     ],
-    'permission_structure' => [
-        'cru_user' => [
-            'profile' => 'c,r,u'
-        ],
-    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+    // 'permission_structure' => [
+    //     'cru_user' => [
+    //         'profile' => 'c,r,u'
+    //     ],
+    // ],
     
 ];
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,46 +13,42 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
+        $manger = User::create([
             'name'=>'manger',
             'email'=>'manger@app.com',
             'phone'=>'01231545',
             'password'=>bcrypt('1234'),
-            'role'=>'manger',
         ]);
-        $user->attachRole('manger');
-        User::create([
+        $manger->attachRole('manger');
+        $teacher1 = User::create([
             'name'=>'teacher1',
             'email'=>'teacher1@app.com',
             'phone'=>'01241545',
             'password'=>bcrypt('1234'),
-            'role'=>'teacher',
         ]);
-
-        User::create([
+        $teacher1->attachRole('teacher');
+        $teacher2 = User::create([
             'name'=>'teacher2',
             'email'=>'teacher2@app.com',
             'phone'=>'01291545',
             'password'=>bcrypt('1234'),
-            'role'=>'teacher',
         ]);
-
-        User::create([
+        $teacher2->attachRole('teacher');
+        $assistant1 = User::create([
             'name'=>'assistant1',
             'email'=>'assistant1@app.com',
             'phone'=>'02231545',
             'password'=>bcrypt('1234'),
-            'role'=>'assistant',
             'user_id'=>2,
         ]);
-
-        User::create([
+        $assistant1->attachRole('assistant');
+        $assistant2 = User::create([
             'name'=>'assistant2',
             'email'=>'assistant2@app.com',
             'phone'=>'01238545',
             'password'=>bcrypt('1234'),
-            'role'=>'assistant',
             'user_id'=>2,
         ]);
+        $assistant2->attachRole('assistant');
     }
 }
